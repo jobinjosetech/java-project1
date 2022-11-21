5aea3731bbc00976a685dcc452eb853884d06cd8
jobin <jobinjose.in@gmail.com>
2022-11-21 12:19:31 +0530

    project 1

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("------------------ Menu ------------------");
-        System.out.println("1. Find largest of 3 numbers");
-        System.out.println("2. Find smallest of 3 numbers");
-        System.out.println("3. Check prime or not");
-        System.out.println("4. Check even or not");
-        System.out.println("5. Find reverse of a number");
-        System.out.println("6. Exit");
-        System.out.println("-------------------------------------------");
-        System.out.print("Please select a choice: ");
-        int choice = sc.nextInt();
-
+        while(true) {
+            System.out.println("------------------ Menu ------------------");
+            System.out.println("1. Find largest of 3 numbers");
+            System.out.println("2. Find smallest of 3 numbers");
+            System.out.println("3. Check prime or not");
+            System.out.println("4. Check even or not");
+            System.out.println("5. Find reverse of a number");
+            System.out.println("6. Exit");
+            System.out.println("-------------------------------------------");
+            System.out.print("Please select a choice: ");
+            int choice = sc.nextInt();
+        }
     }
 }
